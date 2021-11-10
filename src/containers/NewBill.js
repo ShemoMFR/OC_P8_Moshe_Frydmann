aae09da1343595d34e5dd50a2d898ee813986ebc
Shemo <moshe.frydmann@gmail.com>
2021-11-10 15:32:20 +0200

    bills tested written

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -1,4 +1,3 @@
-/* 
 import { ROUTES_PATH } from '../constants/routes.js'
 import Logout from "./Logout.js"
 
@@ -15,31 +14,38 @@ export default class NewBill {
     this.fileName = null
     new Logout({ document, localStorage, onNavigate })
   }
-
   handleChangeFile = e => {
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0];
-    const filePath = e.target.value.split(/\\/g);
-    const fileName = file.name;
-    const fileExtension = fileName.split(".").pop();
-    if (fileExtension === "png" ||fileExtension === "jpeg" || fileExtension === "jpg") {
-      
-      this.firestore
+    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    const filePath = e.target.value.split(/\\/g)
+    const fileName = filePath[filePath.length-1]
+
+
+    const extensionCheck = /(png|jpg|jpeg)/g;
+    const extension = fileName.split(".").pop();
+    const matchExtension = extension.toLowerCase().match(extensionCheck);
+
+    
+    this.handleFirestoreStorage(fileName, file, matchExtension);
+ };
+
+  handleFirestoreStorage = (fileName, file, matchExtension) => {
+    if (this.firestore) {
+    this.firestore
       .storage
       .ref(`justificatifs/${fileName}`)
-      .put(file, { contentType: file.type })
-        .then(snapshot => snapshot.ref.getDownloadURL())
-        .then(url => {
-          console.log(url)
-          this.fileUrl = url
-          this.fileName = fileName
-      })
+      .put(file)
+      .then(snapshot => snapshot.ref.getDownloadURL())
+      .then(url => {
+        this.fileUrl = url
+        this.fileName = matchExtension ? fileName : "invalid";
+      });
     }
-    else return false;
-     
-  }
-  
+  };      
+ 
+
   handleSubmit = e => {
     e.preventDefault()
+    if (this.fileName === "invalid") return;
     console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
     const email = JSON.parse(localStorage.getItem("user")).email
     const bill = {
@@ -71,86 +77,4 @@ export default class NewBill {
       .catch(error => error)
     }
   }
-} */
-
-import { ROUTES_PATH } from "../constants/routes.js";
-import Logout from "./Logout.js";
-
-export default class NewBill {
-  constructor({ document, onNavigate, firestore, localStorage }) {
-    this.document = document;
-    this.onNavigate = onNavigate;
-    this.firestore = firestore;
-    const formNewBill = this.document.querySelector(
-      `form[data-testid="form-new-bill"]`
-    );
-    formNewBill.addEventListener("submit", this.handleSubmit);
-    const file = this.document.querySelector(`input[data-testid="file"]`);
-    file.addEventListener("input", this.handleChangeFile);
-    this.fileUrl = null;
-    this.fileName = null;
-    new Logout({ document, localStorage, onNavigate });
-  }
-  handleChangeFile = (e) => {
-    const file = this.document.querySelector(`input[data-testid="file"]`)
-      .files[0];
-    const filePath = e.target.value.split(/\\/g);
-    const fileName = filePath[filePath.length - 1];
-    const fileExtension = fileName.split(".").pop();
-    if (
-      fileExtension === "png" || fileExtension === "PNG" || fileExtension === "JPEG" ||
-      fileExtension === "jpeg" || fileExtension === "JPG" ||
-      fileExtension === "jpg"
-    ) {
-      this.firestore.storage
-        .ref(`justificatifs/${fileName}`)
-        .put(file)
-        .then((snapshot) => console.log(snapshot.ref.getDownloadURL()))
-        .then((url) => {
-          this.fileUrl = url;
-          this.fileName = fileName;
-        });
-    } else return false;
-  };
-  handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(
-      'e.target.querySelector(`input[data-testid="datepicker"]`).value',
-      e.target.querySelector(`input[data-testid="datepicker"]`).value
-    );
-    const email = JSON.parse(localStorage.getItem("user")).email;
-    const bill = {
-      email,
-      type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
-      amount: parseInt(
-        e.target.querySelector(`input[data-testid="amount"]`).value
-      ),
-      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
-      vat: e.target.querySelector(`input[data-testid="vat"]`).value,
-      pct:
-        parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) ||
-        20,
-      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`)
-        .value,
-      fileUrl: this.fileUrl,
-      fileName: this.fileName,
-      status: "pending",
-    };
-    this.createBill(bill);
-    this.onNavigate(ROUTES_PATH["Bills"]);
-  };
-
-  // not need to cover this function by tests
-  createBill = (bill) => {
-    if (this.firestore) {
-      this.firestore
-        .bills()
-        .add(bill)
-        .then(() => {
-          this.onNavigate(ROUTES_PATH["Bills"]);
-        })
-        .catch((error) => error);
-    }
-  };
 }
